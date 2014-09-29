e45b2421d2fe0a2ade227cd1b5ecb0bcf3a61183
Petrik van der Velde <petrikvandervelde@gmail.com>
2014-09-29 23:40:28 +1300

    Adding the Packer configuration and the main Powershell script that will be used to initialize the machine.

---
+++ scripts/Initialize-JenkinsMaster.ps1
@@ -0,0 +1,27 @@
+Write-Output "Starting initialization of Jenkins master ..."
+
+# Enable server roles:
+# - Email?
+
+# Install .NET 3.5
+
+# Install JRE
+
+# Install GIT
+
+# Install Jenkins
+
+# Install Jenkins plugins
+
+# Update Jenkins config to redirect workspaces and build data
+# workspaces -> "c:\integration\workspaces"
+# build data -> "c:\integration\builds"
+
+# Mount the data VHD as "D:"
+
+# Create Junction for jobs ("c:\integration\jenkins\jobs")
+
+# Create Junction for build data ("c:\integration\builds")
+
+
+Write-Output "Initialization of Jenkins master complete."
